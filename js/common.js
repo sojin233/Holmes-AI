e8fb6ca854c8aca7a445caf1bc85fe7f9112daef
sojin <sojin233@naver.com>
2024-07-11 15:04:25 +0900

    moblie nav 추가

--- js/common.js
+++ js/common.js
@@ -3,4 +3,22 @@ $(document).ready(function () {
   $(".up_btn").bind("click", function () {
     $("html, body").animate({ scrollTop: "0" }, 680);
   });
+
+  // nav function
+  $(".btn-menu").click(function () {
+    if ($(".btn-menu > .ico-nav").hasClass("open")) {
+      $(".btn-menu > .ico-nav").removeClass("open");
+      $(".nav-mobile").fadeOut();
+      $("html").attr("style", "overflow: auto !important");
+    } else {
+      $(".btn-menu > .ico-nav").addClass("open");
+      $(".nav-mobile").fadeIn();
+      $(".nav-mobile .content-box > ul > li").removeClass("open");
+      $("html").attr("style", "overflow: hidden !important");
+    }
+  });
+  $(".nav-mobile .content-box > ul > li").click(function () {
+    $(this).toggleClass("open");
+    $(this).siblings().removeClass("open");
+  });
 });
