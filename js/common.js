75a1ff729632c3747330e761d8bc876ba02c6b89
sojin <sojin233@naver.com>
2024-07-11 13:39:42 +0900

    up button추가

--- js/common.js
+++ js/common.js
@@ -1,1 +1,6 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // button click 위로 올라가게
+  $(".up_btn").bind("click", function () {
+    $("html, body").animate({ scrollTop: "0" }, 680);
+  });
+});
